5d0c66b9a1adad7a04bbe212ea605e4c7a71ffaa
Emirhan Kılıçaslan <emirhannkilicaslann@gmail.com>
2022-02-08 10:45:21 +0300

    08.02.2022

---
+++ OOP3/KrediManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediManager  //birbirinin alternatifi olan ama kodları farklı olan şeylerde.
+    {
+        void Hesapla();
+        void BiseyYap();
+    }
+}
